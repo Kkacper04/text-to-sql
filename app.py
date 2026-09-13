da2dd20d1f829964d52005114b01d5906602fed9
Kkacper04 <kacpicho@gmail.com>
2026-09-13 22:55:47 +0200

    Add initial implementation of Text-to-SQL Agent interface

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+from agent import process_query
+
+st.set_page_config(page_title="Text-to-SQL Agent", page_icon=":robot_face:", layout="wide")
+st.title("Text-to-SQL Agent")
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        if "dataframe" in msg:
+            st.dataframe(msg["dataframe"])
+        else:
+            st.markdown(msg["content"])
+
+if user_prompt := st.chat_input("Ask a question in simple text"):
+    with st.chat_message("user"):
+        st.markdown(user_prompt)
+    st.session_state.messages.append({"role": "user", "content": user_prompt})
+
+    with st.chat_message("assistant"):
+        with st.status("Processing query") as status:
+            result_dict = process_query(user_prompt)
+
+            for msg in result_dict.get("history", []):
+                st.write(msg)
+            status.update(label="Processing query... Done",state="complete",expanded=False)
+
